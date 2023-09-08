1add8c7e8ce68b00e89541ae610224a64e61f01a
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-08 19:43:14 -0300

    feat: create register page

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,7 +1,51 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/input/Input";
+import { Button } from "../../components/button/Button";
+
 export function Register() {
   return (
-    <h1>
-      Register page
-    </h1>
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold tracking-[-0.1px]">
+          Crie sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
+          <Link
+            to="/login"
+            className="tracking-[-0.5px] font-medium text-teal-900"
+          >
+            Fazer Login
+          </Link>
+        </p>
+
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input
+          placeholder="Nome"
+          name="name"
+        />
+
+        <Input
+          type="email"
+          placeholder="E-mail"
+          name="email"
+        />
+
+        <Input
+          type="password"
+          placeholder="Senha"
+          name="password"
+        />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
   )
 }
