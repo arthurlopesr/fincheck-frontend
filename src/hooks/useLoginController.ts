a48287c3a374e0321c132c088a152600130487d2
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-10 18:56:12 -0300

    feat: create useLoginCrontroller with zod and rhf

---
+++ src/hooks/useLoginController.ts
@@ -0,0 +1,26 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().nonempty('E-mail é obrigatório').email('Informe um e-mail válido'),
+  password: z.string().nonempty('Senha é obrigatória').min(8, 'A senha deve ter no mínimo 8 caracteres'),
+})
+
+type FormData = z.infer<typeof schema>;
+
+export function useLoginController() {
+  const {
+    register,
+    handleSubmit: hookFormHandleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleSubmit = hookFormHandleSubmit((data) => {
+    console.log('Chama a API com:', data);
+  });
+
+  return { handleSubmit, register, errors };
+}
