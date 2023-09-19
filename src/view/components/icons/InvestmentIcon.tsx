47f992cec64230f1a4a1773810b4ac3602fd1d85
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-19 20:15:32 -0300

    feat: create new icons

---
+++ src/view/components/icons/InvestmentIcon.tsx
@@ -0,0 +1,18 @@
+export function InvestmentIcon() {
+  return (
+    <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="1" y="1" width="42" height="42" rx="21" fill="#E9ECEF" />
+      <path d="M15.52 30.274V28.354" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M16.2547 28.354H14.7845C13.9981 28.354 13.3601 27.7183 13.3601 26.934V22.0929C13.3601 21.3085 13.9981 20.674 14.7845 20.674H16.2547C17.0421 20.674 17.68 21.3085 17.68 22.0929V26.934C17.68 27.7183 17.0421 28.354 16.2547 28.354Z" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M15.52 20.674V18.7539" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M22 26.4339V24.0339" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M22.7347 24.034H21.2645C20.478 24.034 19.8401 23.3984 19.8401 22.614V21.6129C19.8401 20.8285 20.478 20.194 21.2645 20.194H22.7347C23.5221 20.194 24.16 20.8285 24.16 21.6129V22.614C24.16 23.3984 23.5221 24.034 22.7347 24.034Z" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M22 20.194V14.9139" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M28.48 23.5539V21.1539" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M29.2147 21.154H27.7445C26.958 21.154 26.3201 20.5184 26.3201 19.734V16.8129C26.3201 16.0285 26.958 15.3939 27.7445 15.3939H29.2147C30.0021 15.3939 30.64 16.0285 30.64 16.8129V19.734C30.64 20.5184 30.0021 21.154 29.2147 21.154Z" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M28.48 15.3939V12.9939" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <rect x="1" y="1" width="42" height="42" rx="21" stroke="white" strokeWidth="2" />
+    </svg>
+
+  )
+}
