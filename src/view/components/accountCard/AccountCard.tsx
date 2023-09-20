306b97930cbbb4994799babc335ba86173125820
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-19 22:05:48 -0300

    feat: add account card component

--- src/view/components/accountCard/AccountCard.tsx
+++ src/view/components/accountCard/AccountCard.tsx
@@ -1,20 +1,22 @@
 import { formatCurrency } from "../../../app/utils/formartCurrency";
-import { CategoryIcon } from "../icons/categories/CategoryIcon";
+import { BankAccountTypeIcon } from "../icons/BankAccountTypeIcon";
 
 interface AccountCardProps {
   color: string;
   name: string;
   balance: number;
+  type: 'CASH' | 'CHECKING' | 'INVESTIMENT';
 }
 
-export function AccountCard({ color, name, balance }: AccountCardProps) {
+export function AccountCard({ color, name, balance, type }: AccountCardProps) {
   return (
     <div
       className="p-4 bg-white rounded-2xl h-[200px] flex flex-col justify-between border-b-4 border-teal-950"
       style={{ borderColor: color }}
     >
       <div>
-        <CategoryIcon type="income" />
+        <BankAccountTypeIcon type={type} />
+
         <span className="text-gray-800 font-medium tracking-[-0.5px] mt-4 block">
           {name}
         </span>
