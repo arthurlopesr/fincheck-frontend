3ed40aa85547df120d68bcc8c44cf2b3b168801e
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-26 21:38:05 -0300

    feat: initial transactions page

--- src/view/components/transactions/Transactions.tsx
+++ src/view/components/transactions/Transactions.tsx
@@ -1,7 +1,35 @@
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { TransactionsIcon } from "../icons/TransactionsIcon";
+import { FilterIcon } from "../icons/FilterIcon";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 export function Transactions() {
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full p-10">
-      Transactions
+      <header className="">
+        <div className="flex items-center justify-between">
+          <button className="flex items-center gap-2">
+            <TransactionsIcon />
+            <span className="text-sm text-gray-800 tracking-[-0.5] font-medium"> Transações </span>
+            <ChevronDownIcon className="text-gray-900" />
+          </button>
+
+          <button>
+            <FilterIcon />
+          </button>
+        </div>
+
+        <div className="mt-6">
+          <Swiper>
+            <SwiperSlide>
+
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </header>
+      <div className="mt-4">
+        Conteudo
+      </div>
     </div>
   )
 }
