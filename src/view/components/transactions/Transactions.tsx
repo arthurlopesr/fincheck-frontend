6558ef1069b060544e1ada15a7db4220e99ed81a
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-27 20:24:55 -0300

    feat: create months slider in transactions section

--- src/view/components/transactions/Transactions.tsx
+++ src/view/components/transactions/Transactions.tsx
@@ -2,6 +2,9 @@ import { ChevronDownIcon } from "@radix-ui/react-icons";
 import { TransactionsIcon } from "../icons/TransactionsIcon";
 import { FilterIcon } from "../icons/FilterIcon";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { MONTHS } from "../../../app/config/constants";
+import { SliderOption } from "./SliderOption";
+import { SliderNavigation } from "./SliderNavigation";
 
 export function Transactions() {
   return (
@@ -10,23 +13,37 @@ export function Transactions() {
         <div className="flex items-center justify-between">
           <button className="flex items-center gap-2">
             <TransactionsIcon />
-            <span className="text-sm text-gray-800 tracking-[-0.5] font-medium"> Transações </span>
+            <span className="text-sm text-gray-800 tracking-[-0.5] font-medium">
+              Transações
+            </span>
             <ChevronDownIcon className="text-gray-900" />
           </button>
-
           <button>
             <FilterIcon />
           </button>
         </div>
 
-        <div className="mt-6">
-          <Swiper>
-            <SwiperSlide>
-
-            </SwiperSlide>
+        <div className="mt-6 relative">
+          <Swiper
+            slidesPerView={3}
+            centeredSlides
+          >
+            <SliderNavigation />
+            {MONTHS.map((month, index) => (
+              <SwiperSlide key={month}>
+                {({ isActive }) => (
+                  <SliderOption
+                    index={index}
+                    month={month}
+                    isActive={isActive}
+                  />
+                )}
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </header>
+
       <div className="mt-4">
         Conteudo
       </div>
