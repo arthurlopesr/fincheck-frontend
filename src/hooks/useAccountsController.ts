a59216866e205e51f399d0e62284be3fb3e99b83
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-27 21:39:55 -0300

    feat: add useDashboard

--- src/hooks/useAccountsController.ts
+++ src/hooks/useAccountsController.ts
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useWindowWidth } from "./useWindowWidth";
+import { useDashboard } from "./useDashboard";
 
 export function useAccountsController() {
   const windowWidth = useWindowWidth();
+  const { areValuesVisible, toggleValuesVisibility } = useDashboard();
 
   const [sliderState, setSliderState] = useState({
     isBeginning: true,
@@ -13,5 +15,7 @@ export function useAccountsController() {
     sliderState,
     setSliderState,
     windowWidth,
+    areValuesVisible,
+    toggleValuesVisibility,
   }
 }
