24e5d0a8d3ee4ef78fea8e3dbbfa70b7ee892974
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-29 21:01:44 -0300

    feat: created functions when selecting and other styles

--- src/view/components/dropDownMenu/DropDownMenu.tsx
+++ src/view/components/dropDownMenu/DropDownMenu.tsx
@@ -1,4 +1,5 @@
 import * as RdxDropdownMenu from '@radix-ui/react-dropdown-menu';
+import { cn } from '../../../app/utils/cn';
 
 export function DropdownMenuRoot({ children }: { children: React.ReactNode }) {
   return (
@@ -18,11 +19,19 @@ export function DropdownMenuTrigger({ children }: { children: React.ReactNode })
   )
 }
 
-export function DropdownMenuContent({ children }: { children: React.ReactNode }) {
+interface DropdownMenuContentProps {
+  children: React.ReactNode,
+  className?: string
+}
+
+export function DropdownMenuContent({ children, className }: DropdownMenuContentProps) {
   return (
     <RdxDropdownMenu.Portal>
       <RdxDropdownMenu.Content
-        className='rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10);]'
+        className={cn(
+          'rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10);]',
+          className
+        )}
       >
         {children}
       </RdxDropdownMenu.Content>
@@ -30,11 +39,20 @@ export function DropdownMenuContent({ children }: { children: React.ReactNode })
   )
 }
 
+interface DropdownMenuItemProps {
+  children: React.ReactNode
+  className?: string
+  onSelect?: () => void
+}
 
-export function DropdownMenuItem({ children }: { children: React.ReactNode }) {
+export function DropdownMenuItem({ children, className, onSelect }: DropdownMenuItemProps) {
   return (
     <RdxDropdownMenu.Item
-      className='outline-none min-h-[48px] flex items-center p-4 text-sm text-gray-800 hover:bg-gray-50 rounded-2xl transition-colors'
+      onSelect={onSelect}
+      className={cn(
+        'outline-none min-h-[48px] flex items-center p-4 text-sm text-gray-800 data-[highlighted]:bg-gray-50 rounded-2xl transition-colors cursor-pointer',
+        className
+      )}
     >
       {children}
     </RdxDropdownMenu.Item>
