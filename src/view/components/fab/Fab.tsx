4cd13030bfbc4df43201c017986b5053a998d0ea
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-29 21:48:55 -0300

    feat: crate fab component

---
+++ src/view/components/fab/Fab.tsx
@@ -0,0 +1,35 @@
+import { PlusIcon } from "@radix-ui/react-icons";
+import { DropdownMenu } from "../dropDownMenu/DropDownMenu";
+import { CategoryIcon } from "../icons/categories/CategoryIcon";
+import { BankAccountIcon } from "../icons/BankAccountIcon";
+
+export function Fab() {
+  return (
+    <div className="fixed bottom-4 right-4">
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <button className="w-12 h-12 bg-teal-900 rounded-full shadow-lg flex items-center justify-center text-white ">
+            <PlusIcon className="w-6 h-6" />
+          </button>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content>
+          <DropdownMenu.Item className="gap-2">
+            <CategoryIcon type="expense" />
+            Nova Despesa
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item className="gap-2">
+          <CategoryIcon type="income" />
+            Nova Receita
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item className="gap-2">
+            <BankAccountIcon />
+            Nova Conta
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </div>
+  )
+}
---
+++ src/view/components/transactions/TransactionTypeDropDown.tsx
@@ -0,0 +1,38 @@
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { TransactionsIcon } from "../icons/TransactionsIcon";
+import { DropdownMenu } from "../dropDownMenu/DropDownMenu";
+import { IncomeIcon } from "../icons/IncomeIcon";
+import { ExpensesIcon } from "../icons/ExpensesIcon";
+
+export function TransactionTypeDropDown() {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <button className="flex items-center gap-2">
+          <TransactionsIcon />
+          <span className="text-sm text-gray-800 tracking-[-0.5] font-medium">
+            Transações
+          </span>
+          <ChevronDownIcon className="text-gray-900" />
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-[279px]">
+        <DropdownMenu.Item className="gap-2">
+          <IncomeIcon />
+          Receitas
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item className="gap-2">
+          <ExpensesIcon />
+          Despesas
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item className="gap-2">
+          <TransactionsIcon />
+          Transações
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  )
+}
