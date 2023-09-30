b7afd612562be7c3ad5458ac9bdb3f1f9adc4bb3
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-29 21:19:58 -0300

    fix: fixing z index

--- src/view/components/dropDownMenu/DropDownMenu.tsx
+++ src/view/components/dropDownMenu/DropDownMenu.tsx
@@ -29,7 +29,7 @@ export function DropdownMenuContent({ children, className }: DropdownMenuContent
     <RdxDropdownMenu.Portal>
       <RdxDropdownMenu.Content
         className={cn(
-          'rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10);] data-[side=bottom]:animate-slide-up-and-fade',
+          'rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10);] data-[side=bottom]:animate-slide-up-and-fade z-50',
           className
         )}
       >
