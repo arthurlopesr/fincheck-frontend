341a2fa372d70f9bf3415d89063b5ad6532c2759
Arthur Lopes <arthurlopr12@gmail.com>
2023-10-05 21:09:42 -0300

    feat: add react hook form at custom hook

--- src/hooks/controllers/useNewAccountModalController.ts
+++ src/hooks/controllers/useNewAccountModalController.ts
@@ -1,4 +1,16 @@
+import { z } from "zod";
 import { useDashboard } from "../useDashboard";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  initialBalance: z.string().nonempty('Saldo inicial é obrigatório'),
+  name: z.string().nonempty('Nome da conta é obrigatório'),
+  type: z.enum(['CHECKING', 'INVESTMENT', 'CASH']),
+  color: z.string().nonempty('Cor é obrigatória'),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function useNewAccountModalController() {
   const {
@@ -6,8 +18,26 @@ export function useNewAccountModalController() {
     closeNewAccountModal,
   } = useDashboard();
 
+  const {
+    register,
+    handleSubmit: hookformSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleSubmit = hookformSubmit(async (data) => {
+    console.log(data);
+
+  });
+
   return {
     isNewAccountModalOpen,
     closeNewAccountModal,
+    register,
+    errors,
+    handleSubmit,
+    control,
   }
 }
