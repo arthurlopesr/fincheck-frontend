276db5ac9143355bf3838e458c9aa0409013f8a1
Arthur Lopes <arthurlopr12@gmail.com>
2023-10-08 18:28:49 -0300

    refactor: interface name

--- src/app/services/bankAccountsService/create.ts
+++ src/app/services/bankAccountsService/create.ts
@@ -1,13 +1,13 @@
 import { httpClient } from "../httpClient";
 
-export interface BankAccountParams {
+export interface CreateBankAccountParams {
   name: string,
   initialBalance: number,
   color: string,
   type: 'CHECKING' | 'INVESTIMENT' | 'CASH';
 }
 
-export async function create(params: BankAccountParams) {
+export async function create(params: CreateBankAccountParams) {
   const { data } = await httpClient.post('/bank-accounts', params)
 
   return data;
